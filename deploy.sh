604dabffe4dd693ac744dcf6494c09f30aac7706
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2018-04-28 19:57:25 +0600

    + x32

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 Major=8
-Minor=151
+Minor=171
 Build=12
 
-Jdk=jdk-${Major}.${Minor}.tar.gz
-Jre=jre-${Major}.${Minor}.tar.gz
+Jdk=jdk-${Major}.${Minor}
+Jre=jre-${Major}.${Minor}
 
-wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/e758a0de34e24606bca991d704f6dcbf/jdk-${Major}u${Minor}-linux-x64.tar.gz \
-     --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-     --no-check-certificate \
-     -O ${Jdk}
+function doTheWork {
+     wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/e758a0de34e24606bca991d704f6dcbf/jdk-${Major}u${Minor}-linux-$1.tar.gz \
+          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+          --no-check-certificate \
+          -O ${Jdk}-$1.tar.gz
 
-curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jdk} -v -# \
-     -uaibar:${BintrayKey} \
-     -T ${Jdk}
+     curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jdk}-$1.tar.gz -v -# \
+          -uaibar:${BintrayKey} \
+          -T ${Jdk}-$1.tar.gz
 
-wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/e758a0de34e24606bca991d704f6dcbf/jre-${Major}u${Minor}-linux-x64.tar.gz \
-     --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-     --no-check-certificate \
-     -O ${Jre}
+     wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/e758a0de34e24606bca991d704f6dcbf/jre-${Major}u${Minor}-linux-$1.tar.gz \
+          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+          --no-check-certificate \
+          -O ${Jre}-$1.tar.gz
 
-curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jre} -v -# \
-     -uaibar:${BintrayKey} \
-     -T ${Jre}
+     curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jre}-$1.tar.gz -v -# \
+          -uaibar:${BintrayKey} \
+          -T ${Jre}-$1.tar.gz
+}
