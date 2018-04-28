cee1beaea0f49fcd17df059ee906aee13c2a3b52
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2018-04-28 20:20:24 +0600

    + x32

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,14 @@ Major=8
 Minor=171
 Build=11
 
-Jdk=jdk-${Major}.${Minor}
-Jre=jre-${Major}.${Minor}
+Version=${Major}.${Minor}
 
-function doTheWork {
-     wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/512cd62ec5174c3487ac17c61aaa89e8/jdk-${Major}u${Minor}-linux-$1.tar.gz \
-          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-          --no-check-certificate \
-          --no-cookies \
-          -O ${Jdk}-$1.tar.gz
+wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/512cd62ec5174c3487ac17c61aaa89e8/${Type}-${Major}u${Minor}-linux-${Arc}.tar.gz \
+     --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+     --no-check-certificate \
+     --no-cookies \
+      -O ${Type}-${Version}-${Arc}.tar.gz
 
-     curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jdk}-$1.tar.gz -v -# \
-          -uaibar:${BintrayKey} \
-          -T ${Jdk}-$1.tar.gz
-
-     wget http://download.oracle.com/otn-pub/java/jdk/${Major}u${Minor}-b${Build}/512cd62ec5174c3487ac17c61aaa89e8/jre-${Major}u${Minor}-linux-$1.tar.gz \
-          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-          --no-check-certificate \
-          -O ${Jre}-$1.tar.gz
-
-     curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Jre}-$1.tar.gz -v -# \
-          -uaibar:${BintrayKey} \
-          -T ${Jre}-$1.tar.gz
-}
-
-doTheWork 'x64'
-doTheWork 'x32'
+curl https://api.bintray.com/content/walkingdevs/mirrors/oraclejdk/${Major}.${Minor}/${Type}-${Arc}.tar.gz -v -# \
+     -uaibar:${BintrayKey} \
+     -T ${Type}-${Version}-${Arc}.tar.gz
