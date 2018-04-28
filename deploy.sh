6b85eeb531b07dd57aad8f275ac3443c0d0c2bfc
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2018-04-28 20:34:35 +0600

    + x32

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 Type='jdk' Arc='x64' ./work.sh
-Type='jdk' Arc='x32' ./work.sh
+Type='jdk' Arc='i586' ./work.sh
+
 Type='jre' Arc='x64' ./work.sh
-Type='jre' Arc='x32' ./work.sh
+Type='jre' Arc='i586' ./work.sh
